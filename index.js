2a9616f4c4010cfc59967248529c774691395596
TrevorPeters <TrevorPeters@users.noreply.github.com>
2020-09-23 16:08:37 -0700

    added basic game functionality

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 import {GameBoard} from './GameBoard.js'
 
-document.getElementById("start").onclick = function(){
-    var playerOne = document.getElementById("playerOne").value;
-    var playerTwo = document.getElementById("playerTwo").value;
-    var AI = document.getElementById("AI").value;
-    var playerOneToken = document.getElementById("playerOneToken").value.toLowerCase();
-    var playerTwoToken = (playerOneToken == "w") ? "b" : "w";
+var playerOne;
+var playerTwo;
+var AI;
+var playerOneToken;
+var playerTwoToken;
+var Game = GameBoard;
+var turn = 0;
+document.getElementById("start").onclick = function Initialize() {
+    playerOne = document.getElementById("playerOne").value;
+    playerTwo = document.getElementById("playerTwo").value;
+    AI = document.getElementById("AI").value;
+    playerOneToken = document.getElementById("playerOneToken").value.toLowerCase();
+    playerTwoToken = (playerOneToken == "w") ? "b" : "w";
     //alert("text " + playerTwo + playerOne + AI + playerOneToken + playerTwoToken);
-    var Game = GameBoard;
-    //Game.rotate("4R");
-    alert(Game.checkWinner());
-    //alert(Game.bWon + "\n" + Game.wWon);
+    //alert(Game.checkWinner());
     document.getElementById("GameBoardDisplay").innerHTML = Game.toString();  
+}
+
+document.getElementById("move").onclick = function move() {
+    var pos = document.getElementById("position").value;
+    var rot = document.getElementById("rotation").value;
+    var token = (turn == 0) ? playerOneToken : playerTwoToken;
+    turn = (turn + 1) % 2;
+    Game.insertToken(token, pos);
+    Game.checkWinner();
+    Game.rotate(rot);
+    Game.checkWinner();
+    document.getElementById("GameBoardDisplay").innerHTML = Game.toString();
 }
